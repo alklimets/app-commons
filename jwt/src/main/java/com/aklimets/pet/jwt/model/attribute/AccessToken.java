e91e93c7be9cd6b4bcdfa891859bc1c01e5052b8
aliaksandrklimets <aliaksandrklimets@gmail.com>
2024-09-17 23:36:24 +0200

    small updates

--- jwt/src/main/java/com/aklimets/pet/jwt/model/attribute/AccessToken.java
+++ jwt/src/main/java/com/aklimets/pet/jwt/model/attribute/AccessToken.java
@@ -1,13 +1,10 @@
 package com.aklimets.pet.jwt.model.attribute;
 
 import com.aklimets.pet.buildingblock.interfaces.DomainAttribute;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
@@ -15,7 +12,5 @@ import jakarta.validation.constraints.NotNull;
 public class AccessToken extends DomainAttribute<String> {
 
     @NotNull
-    @Min(value = 3, message = "Password should be min 3 characters long")
-    @Max(value = 25, message = "Password should be max 25 characters long")
     private String value;
 }
--- jwt/src/main/java/com/aklimets/pet/jwt/model/attribute/RefreshToken.java
+++ jwt/src/main/java/com/aklimets/pet/jwt/model/attribute/RefreshToken.java
@@ -1,14 +1,11 @@
 package com.aklimets.pet.jwt.model.attribute;
 
 import com.aklimets.pet.buildingblock.interfaces.DomainAttribute;
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-
-import jakarta.persistence.Embeddable;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 
 @Embeddable
 @Getter
@@ -17,8 +14,6 @@ import jakarta.validation.constraints.NotNull;
 public class RefreshToken extends DomainAttribute<String> {
 
     @NotNull
-    @Min(value = 3, message = "Password should be min 3 characters long")
-    @Max(value = 25, message = "Password should be max 25 characters long")
     private String value;
 
     protected RefreshToken() {
